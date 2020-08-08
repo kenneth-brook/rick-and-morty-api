4394433810ecc1e8e2c06b7798c2c202f5254440
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-08 13:46:47 -0400

    old project moved over

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./sass-master/index.scss";
+import { Route } from "react-router-dom";
 
-function App() {
+import TabNav from "./components/TabNav.js";
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList";
+import LocationsList from "./components/LocationsList";
+import EpisodesList from "./components/EpisodesList";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Here we go again</h1>
-    </div>
+    <main>
+      <Header />
+      <TabNav />
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/characters" component={CharacterList} />
+      <Route exact path="/locations" component={LocationsList} />
+      <Route exact path="/episodes" component={EpisodesList} />
+    </main>
   );
 }
-
-export default App;
---
+++ src/components/SearchForm.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+export default function SearchForm({ onSearch }) {
+  return (
+    <section className="search-form">
+      <form onSubmit={() => onSearch(name)}>
+        <input
+          onChange={handleInputChange}
+          placeholder="name"
+          value={name}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </section>
+  );
+}
