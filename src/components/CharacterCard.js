624ac166c555e6e9ca23c50305eb0519354a99b8
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-08-17 23:52:41 -0400

    working on next prev function

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,12 +3,16 @@ import React from "react";
 export default function CharacterCard(props) {
   return (
     <div className="card">
-      <img src={props.image} />
-      <h3>{props.name}</h3>
-      <p style={{ color: "silver" }}>{props.status}</p>
-      <p>Location: {props.location.name}</p>
-      <br />
-      <p>Origin: {props.origin.name}</p>
+      <div className="imageWrap">
+        <img src={props.image} alt={props.name} />
+      </div>
+      <div className="contentWrap">
+        <h3>{props.name}</h3>
+        <p style={{ color: "silver" }}>{props.status}</p>
+        <p>Location: {props.location.name}</p>
+        <br />
+        <p>Origin: {props.origin.name}</p>
+      </div>
     </div>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
 import axios from "axios";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+  let pageAddress = "https://rickandmortyapi.com/api/character/";
 
   useEffect(() => {
-    axios.get("https://rickandmortyapi.com/api/character/").then((res) => {
+    axios.get(`${pageAddress}`).then((res) => {
       setCharacters(res.data.results);
+      setInfo(res.data.info);
     });
-  }, []);
+  }, [pageAddress]);
 
-  console.log(characters);
+  console.log(info.next);
   return (
     <section className="character-list grid-view">
+      <div className="pageNav">
+        <button className="arrowLeft">
+          <h1>Previous</h1>
+          <h1>
+            <FaArrowLeft />
+          </h1>
+        </button>
+        <button className="arrowRight" onClick={(pageAddress = info.next)}>
+          <h1>Next</h1>
+          <h1>
+            <FaArrowRight />
+          </h1>
+        </button>
+      </div>
       {characters.map((char) => {
         return (
           <CharacterCard
